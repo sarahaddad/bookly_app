da2af86d21703473ddb22d433ff789fba1e2cb33
Sarah_alhajmuhammed <75433758+sarahaddad@users.noreply.github.com>
2023-04-09 00:23:14 +0300

    create home repo methods and implementaion

--- lib/features/home/data/repos/home-repo_implemention.dart
+++ lib/features/home/data/repos/home-repo_implemention.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+import 'package:ebook/core/utils/api_service.dart';
 import 'package:ebook/features/home/data/models/book_model/book_model.dart';
 import 'package:ebook/core/errors/failures.dart';
 import 'package:dartz/dartz.dart';
@@ -5,15 +7,56 @@ import 'package:ebook/features/home/data/repos/home_repo.dart';
 import 'package:ebook/features/home/presentaion/views/widgets/home_page_body.dart';
 
 class HomeRepoImp implements HomeRepo {
+  final ApiServices apiServices;
+
+  HomeRepoImp(this.apiServices);
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiServices.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest &q=computer science');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      if (e is DioError) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeturedBooks() {
-    // TODO: implement fetchFeturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeturedBooks() async {
+    try {
+      var data = await apiServices.get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=subject:Programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      if (e is DioError) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
   }
 }
--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -3,6 +3,6 @@ import 'package:ebook/core/errors/failures.dart';
 import 'package:ebook/features/home/data/models/book_model/book_model.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks();
   Future<Either<Failure, List<BookModel>>> fetchFeturedBooks();
 }
