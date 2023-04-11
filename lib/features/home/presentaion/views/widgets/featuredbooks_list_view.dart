95139cdf906cafc6346cfae73776f35002c36810
Sarah_alhajmuhammed <75433758+sarahaddad@users.noreply.github.com>
2023-04-12 00:04:59 +0300

    fetured books list view

---
+++ lib/features/home/presentaion/views/widgets/featuredbooks_list_view.dart
@@ -0,0 +1,43 @@
+import 'package:ebook/core/widgets/custom_progress_indicator.dart';
+import 'package:ebook/features/home/presentaion/managers/featured_books_cubit/featured_books_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../../core/widgets/custom_error_widget.dart';
+import 'custom_book_image.dart';
+
+class FeturedBooksListView extends StatelessWidget {
+  const FeturedBooksListView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+        builder: (context, state) {
+      if (state is FeaturedBooksSuccses) {
+        return SizedBox(
+          height: MediaQuery.of(context).size.height * .3,
+          child: ListView.builder(
+            itemCount: state.books.length,
+            physics: const BouncingScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            itemBuilder: ((context, index) => Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 4),
+                  child: CustomBookImage(
+                    imageUrl:
+                        state.books[index].volumeInfo.imageLinks.thumbnail,
+                  ),
+                )),
+          ),
+        );
+      } else if (state is FeaturedBooksFailure) {
+        return CustomErorWidget(
+          errorMsg: state.errMsg,
+        );
+      } else {
+        return const CustomLoadingIndicator();
+      }
+    });
+  }
+}
